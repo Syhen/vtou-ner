4d4fce36c4ebfc8e8543051470a57f92c9b0947e
heyao <heyao@haimaqingfan.com>
2018-12-28 13:24:14 +0800

    add: add text to model input data

--- vtou_ner/preprocessing/corpus.py
+++ vtou_ner/preprocessing/corpus.py
@@ -8,6 +8,9 @@ from collections import Counter
 from collections import OrderedDict
 from itertools import chain
 
+from vtou_ner.preprocessing.sequence import pad_sequence
+from vtou_ner.preprocessing.text import chars2ids
+
 
 def build_vocab(char_lists, top_k=None):
     """build vocab from words list
@@ -28,6 +31,20 @@ def build_char2index(vocab):
 
 def build_index2char(vocab):
     return {i: char for i, char in enumerate(vocab.keys(), 1)}
+
+
+def content2id(data, vocab=None):
+    if vocab is None:
+        vocab = build_vocab(data, top_k=None)
+    char2index = build_char2index(vocab)
+    data = chars2ids(data, char2index)
+    return data
+
+
+def prepare_model_input(text, vocab, maxlen=60, left_pad=False, padding_val=0):
+    chars = list(text)
+    char_ids = [vocab.get(i, 0) for i in chars]
+    return pad_sequence(char_ids, maxlen=maxlen, left_pad=left_pad, padding_val=padding_val)
 
 
 if __name__ == '__main__':
